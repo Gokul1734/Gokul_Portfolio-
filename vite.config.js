2ec4a5e449744422abf847d3ec42d8cc1d422146
Gokulpriyan Karthikeyan <130198919+Gokul1734@users.noreply.github.com>
2024-11-21 09:44:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://gokul-portfolio-backend.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Optional rewrite
-      },
-    },
-  },
-});
-<<<<<<< HEAD
-
-//https://gokul-portfolio-q5jqerma5-gokuls-projects-5ac84439.vercel.app
-=======
->>>>>>> d2e3bbf1fc577b3e1147376e1f9981ebaa63a380
+})
